c0401d86157c87d35afd5a1f153983e13f6e5494
Shinya Tsuda <r.shinya.tsuda@gmail.com>
2026-07-26 09:53:48 +0900

    feat(kayo-player): add local player

--- kayo-player/src/component/MediaChooser.tsx
+++ kayo-player/src/component/MediaChooser.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import { Link, ScrollRestoration, useLoaderData } from 'react-router-dom';
 
 import { GetObjectCommand, type ListObjectsV2CommandOutput, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import CloseIcon from '@mui/icons-material/Close';
 import DescriptionIcon from '@mui/icons-material/Description';
 import FolderIcon from '@mui/icons-material/Folder';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -20,20 +26,36 @@ interface MediaChooserProps {
 function MediaChooser({ s3Client, bucket }: MediaChooserProps) {
   const res = useLoaderData() as ListObjectsV2CommandOutput;
 
-  const play = async (path: string) =>
+  const [showPlayer, setShowPlayer] = useState(false);
+  const [source, setSource] = useState<string | undefined>();
+  const [title, setTitle] = useState<string | undefined>();
+
+  const play = async (path: string, title: string) =>
     await getSignedUrl(s3Client, new GetObjectCommand({
       Bucket: bucket,
       Key: path,
     }), {
       expiresIn: 86400,
     }).then(url => {
-      const media = new window.chrome.cast.media.MediaInfo(url, '');
-      const request = new window.chrome.cast.media.LoadRequest(media);
-      return window.cast.framework.CastContext
+      const session = window.cast.framework.CastContext
         .getInstance()
-        .getCurrentSession()
-        ?.loadMedia(request);
+        .getCurrentSession();
+      if (session) {
+        const media = new window.chrome.cast.media.MediaInfo(url, '');
+        const request = new window.chrome.cast.media.LoadRequest(media);
+        return session.loadMedia(request);
+      } else {
+        setSource(url);
+        setTitle(title);
+        setShowPlayer(true);
+      }
     }).catch(console.error);
+
+  const handleClose = () => {
+    setSource(undefined);
+    setTitle('');
+    setShowPlayer(false);
+  };
 
   return (
     <>
@@ -49,13 +71,33 @@ function MediaChooser({ s3Client, bucket }: MediaChooserProps) {
         ))}
         {res.Contents?.map((o, i) => (
           <ListItem key={i}>
-            <ListItemButton onClick={() => play(o.Key!, basename(o.Key!))}>
+            <ListItemButton onClick={() => play(o.Key!, basename(o.Key!)!)}>
               <ListItemIcon><DescriptionIcon /></ListItemIcon>
               <ListItemText>{basename(o.Key!)}</ListItemText>
             </ListItemButton>
           </ListItem>
         ))}
       </List>
+      <Dialog fullScreen open={showPlayer}>
+        <DialogTitle>{title}</DialogTitle>
+        <IconButton
+          aria-label='close'
+          sx={(theme) => ({
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
+          onClick={handleClose}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent sx={{ overflow: 'hidden' }}>
+          <video autoPlay controls style={{ width: '100%', height: '100%' }}>
+            <source src={source} />
+          </video>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
